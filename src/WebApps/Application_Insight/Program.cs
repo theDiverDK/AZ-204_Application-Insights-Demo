7eb554eaf5ffaddbe7845deb18e58a4fc99dffd8
Søren Reinke <soren@reinke.dk>
2024-05-16 19:29:39 +0200

    test

--- src/WebApps/Application_Insight/Program.cs
+++ src/WebApps/Application_Insight/Program.cs
@@ -11,8 +11,15 @@ builder.Services.AddApplicationInsightsTelemetry();
 
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 
+builder.Services.AddControllersWithViews(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>((module, o) => { module. EnableSqlCommandTextInstrumentation = true; });
 
 var app = builder.Build();
